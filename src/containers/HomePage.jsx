5aa052d30c93fd908317329afaceb575e9e16a84
ledjoshukulli <ledjoshukulli@gmail.com>
2023-08-03 18:16:35 +0200

    home

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,22 +1,55 @@
 import React from 'react';
-import ButtonComponent from '../components/ButtonComponent';
+import { Container, Typography, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import ButtonComponent from '../components/ButtonComponent';
 
 const HomePage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/shop');
+  };
 
-    const handleClick =()=>{
-        navigate('/shop')
-    }
   return (
-    <div>
+    <Box sx={{backgroundColor: '#f5f5f5'}}>
+    <Container
+      maxWidth="md"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Welcome to our Mobile Shop
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Discover the latest smartphones and accessories.
+      </Typography>
+      <Box
+        sx={{
+          marginTop: '30px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography sx={{ marginBottom: '15px' }} variant="h5" gutterBottom>
+          Explore Our Collection
+        </Typography>
         <ButtonComponent
-            content={"SHOP"}
-            handleClick={handleClick}
-        ></ButtonComponent>
-    </div>
-  )
-}
+          content={'Shop now'}
+          variant="contained"
+          color="primary"
+          handleClick={handleClick}
+          sx={{ textTransform: 'none' }} // Adjusting button style
+        />
+      </Box>
+    </Container>
+    </Box>
+  );
+};
 
 export default HomePage;
